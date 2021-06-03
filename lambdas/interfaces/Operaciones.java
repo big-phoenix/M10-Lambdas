8eb8f42707b3bef66a5fbee8e97049d0a131577b
Javi <bisistems@gmail.com>
2021-06-03 12:20:27 +0200

    Nivel 2

---
+++ lambdas/interfaces/Operaciones.java
@@ -0,0 +1,7 @@
+package com.lambdas.interfaces;
+
+public interface Operaciones {
+	
+	public float operacio(int a,int b, String tipo);
+
+}
--- lambdas/main/Main.java
+++ lambdas/main/Main.java
@@ -4,8 +4,10 @@ import java.util.*;
 import java.util.stream.Stream;
 
 import com.lambdas.interfaces.AMayus3;
+import com.lambdas.interfaces.Cadena;
 import com.lambdas.interfaces.Meses;
 import com.lambdas.interfaces.NumParInpar;
+import com.lambdas.interfaces.Operaciones;
 import com.lambdas.interfaces.Pi;
 import com.lambdas.interfaces.Reverse;
 import com.lambdas.interfaces.StringO;
@@ -14,7 +16,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		List<String> nombres = Arrays.asList("Juan","Pepe","Federico","Miguel","Raul","David","Ana","Rodrigo","Rocio");
+		/*List<String> nombres = Arrays.asList("Juan","Pepe","Federico","Miguel","Raul","David","Ana","Rodrigo","Rocio");
 		List<Integer> numeros = Arrays.asList(3,22,50,41,76,11,7,5,6,9);
 		List<String> meses = Arrays.asList("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 				
@@ -46,6 +48,7 @@ public class Main {
 		
 		/******************************************************************************************************/
 		
+		/*
 		Stream<Integer> par = numeros.stream().filter(n -> n % 2 == 0);
 		Stream<Integer> inpar = numeros.stream().filter(n -> n % 2 != 0);
 		
@@ -68,10 +71,12 @@ public class Main {
 		
 		parInpar.parInpar(numeros);*/
 		
+		/*
 		System.out.println("***************************************************************");
 		
 		/******************************************************************************************************/
 		
+		/*
 		StringO letraO = (List<String> cadenas) -> {
 			
 			int total = 0;
@@ -109,6 +114,7 @@ public class Main {
 		
 		/******************************************************************************************************/
 		
+		/*
 		StringO letraO5 = (List<String> cadenas) -> {
 			
 			int total = 0;
@@ -150,11 +156,13 @@ public class Main {
 		
 		/******************************************************************************************************/
 
+		/*
 		meses.forEach(m -> System.out.println("** "+m+" **"));
 		System.out.println("***************************************************************");
 		
 		/******************************************************************************************************/
 		
+		/*
 		meses.forEach(System.out::println);
 		System.out.println("\r\n***************************************************************");
 		
@@ -181,6 +189,119 @@ public class Main {
 		};
 		
 		System.out.println("Palabra original: "+palabra+" parala al reves: "+rever.reverse(palabra));
+		
+		*/
+		
+		
+		System.out.println("******************************* NIVELL 2 ****************************");
+		System.out.println("\n\r******************* Estan alfabeticamnet ****************************");
+		
+		List<String> cadena = Arrays.asList("Camila","4","57","Ana","Federico","84","Patricio","Fernando");
+		
+		Collections.sort(cadena);
+		
+		cadena.forEach(c -> System.out.println(c));
+		
+		System.out.println("\n\r*********************** Estan alfabeticamnet *************************");
+		
+		Comparator<String> alreves = Collections.reverseOrder();
+		Collections.sort(cadena, alreves);
+		
+		cadena.forEach(c -> System.out.println(c));
+		
+		Cadena lista = (List<String> cd) -> {
+			
+			int pos = 0;
+			String aux;
+			
+			for(int i=0; i<cd.size(); i++) {
+				for(int j=0; j<cd.get(i).length(); j++) {
+					if(cd.get(i).charAt(j)=='e') {
+						aux = cd.get(i);
+						cd.set(pos+1, cd.get(i-1));
+						cd.set(pos, aux);
+						pos++;
+					}
+				}
+			}
+			
+			System.out.println("\n\r************************************************");
+			cd.forEach(c -> System.out.println(c));
+			
+		};
+		
+		lista.cadena(cadena);
+		
+		Cadena lista2 = (List<String> cd) -> {
+			
+			for(int i=0; i<cd.size(); i++) {
+		
+				cd.set(i, cd.get(i).replace('a', '4'));
+				
+			}
+			
+			System.out.println("\n\r************************************************");
+			cd.forEach(c -> System.out.println(c));
+			
+		};
+		
+		lista2.cadena(cadena);
+	
+		
+		Cadena lista3 = (List<String> cd) -> {
+			
+			List<Character> n = Arrays.asList('0','1','2','3','4','5','6','7','8','9');
+			
+			for(int i=0; i<cd.size(); i++) {
+		
+				for(int j=0;j<n.size();j++) {
+					
+					if(cd.get(i).charAt(0) == n.get(j)) {
+						System.out.println(cd.get(i));
+					}
+				}
+					
+			}
+						
+		};
+		
+		System.out.println("\n\r************************************************");
+		lista3.cadena(cadena);
+		
+		
+		Operaciones op = (int num, int num2,String tipo) -> {
+			
+			float resultado = 0;
+			
+			switch(tipo) {
+			
+				case "suma":
+					resultado = num+num2;
+				break;
+				
+				case "resta":
+					resultado = num-num2;
+				break;
+				
+				case "multiplicacion":
+					resultado = num*num2;
+				break;
+				
+				case "division":
+					resultado = num/num2;
+				break;
+				
+				default:
+					System.out.println("Introduce un tipo aceptado: ** sumo ** resta ** multiplicacion ** division **");
+			
+			}
+			
+			return resultado;
+			
+		};
+		
+		System.out.println("\n\r****** sumo ** resta ** multiplicacion ** division ************************************");
+		System.out.println(op.operacio(7, 5, "multiplicacion"));
 		
 	}
 
